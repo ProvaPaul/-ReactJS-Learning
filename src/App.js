cdfe2472b1ab39377030a29f287d84c4ad9eb1b4
ProvaPaul <paul2007037@stud.kuet.ac.bd>
2024-10-02 07:25:15 +0600

    distructure

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+import Demo from './components/Demo';
+
+function App() {
+  return (
+    <div className="App">
+      <Demo name="putu" country="BD"/>
+    </div>
+  );
+}
+
+export default App;
